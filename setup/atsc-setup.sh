7ca6ce8fd32f007a383239702309cb48b339cd35
Mikolka <51335952+mikolka9144@users.noreply.github.com>
2025-11-06 00:42:15 +0100

    compress toyCommercial

--- setup/atsc-setup.sh
+++ setup/atsc-setup.sh
@@ -12,8 +12,11 @@ mkdir -p "$DEST_ASTC_DIR";
 
 
 cd assets/atsc_shared/
-find . -name -name "*.astc" -or -name "*.xml" -exec cp -r --parents -t "$DEST_ASTC_DIR" {} +
 find . -name "*.xml" -exec cp -r --parents -t "$DEST_ASTC_DIR" {} +
+cd - > /dev/null
+
+cd astc-textures/assets/atsc_shared/
+find . -name "*" -exec cp -r --parents -t "$DEST_ASTC_DIR" {} +
 cd - > /dev/null
 
 
@@ -23,24 +26,36 @@ cd - > /dev/null
 
 
 cd assets/base_game/
+find characters -name "*"  -exec cp -r --parents -t "$DEST_DIR/images" {} +
+find characters -name "*.json" -exec cp -r --parents -t "$DEST_ASTC_DIR/images" {} +
+cd - > /dev/null
 
-find characters \( -name "*.png" -or -name "*.xml" -or -name "*.json" \)  -exec cp --parents -t "$DEST_DIR/images" {} +
-find characters -name "*.astc" -exec cp -r --parents -t "$DEST_ASTC_DIR/images" {} +
-
+cd astc-textures/assets/base_game/
+find characters -name "*" -exec cp -r --parents -t "$DEST_ASTC_DIR/images" {} +
 cd - > /dev/null
 
 cd assets/base_game/characters_pixel
 find . -name "*" -exec cp -r --parents -t "$DEST_DIR/images/characters" {} +
 cd - > /dev/null
 
-cd assets/base_game/characters
-find . \( -name "*.png" -or -name "*.xml" -or -name "*.json" \) -exec cp -r --parents -t "$DEST_DIR/images/characters" {} +
-find . -name "*.astc" -exec cp -r --parents -t "$DEST_ASTC_DIR/images/characters" {} +
+cd assets/base_game/shared
+find . -name "*" -exec cp -r --parents -t "$DEST_DIR" {} +
+find images -name "*.json" -exec cp -r --parents -t "$DEST_ASTC_DIR" {} +
 cd - > /dev/null
+
+cd astc-textures/assets/base_game/shared
+find . -name "*" -exec cp -r --parents -t "$DEST_ASTC_DIR" {} +
+cd - > /dev/null
+
+
+
 
 cd assets/base_game/week_data
 find . -name "*" -exec cp -r --parents -t "$DEST_DIR" {} +
-find . -name "*.astc" -exec cp -r --parents -t "$DEST_ASTC_DIR" {} +
+cd - > /dev/null
+
+cd astc-textures/assets/base_game/week_data
+find . -name "*" -exec cp -r --parents -t "$DEST_ASTC_DIR" {} +
 cd - > /dev/null
 
 cp "art/mod/pack-astc.json" "$DEST_ASTC_DIR/pack.json";
